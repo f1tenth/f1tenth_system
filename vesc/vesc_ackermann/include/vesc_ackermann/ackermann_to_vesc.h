e9b8e371f94c61f9d71653037ad21a0d74f7864a
billyzheng <billyzheng.bz@gmail.com>
2020-01-28 18:24:52 -0500

    added braking

--- vesc/vesc_ackermann/include/vesc_ackermann/ackermann_to_vesc.h
+++ vesc/vesc_ackermann/include/vesc_ackermann/ackermann_to_vesc.h
@@ -5,6 +5,7 @@
 
 #include <ros/ros.h>
 #include <ackermann_msgs/AckermannDriveStamped.h>
+#include <nav_msgs/Odometry.h>
 
 namespace vesc_ackermann
 {
@@ -20,16 +21,20 @@ private:
   // conversion gain and offset
   double speed_to_erpm_gain_, speed_to_erpm_offset_;
   double steering_to_servo_gain_, steering_to_servo_offset_;
+  double current_vel, vel_diff_thresh;
 
   /** @todo consider also providing an interpolated look-up table conversion */
 
   // ROS services
   ros::Publisher erpm_pub_;
   ros::Publisher servo_pub_;
+  ros::Publisher brake_pub_;
+  ros::Subscriber odom_sub_;
   ros::Subscriber ackermann_sub_;
 
   // ROS callbacks
   void ackermannCmdCallback(const ackermann_msgs::AckermannDriveStamped::ConstPtr& cmd);
+  void odomCallback(const nav_msgs::Odometry::ConstPtr& odom_msg);
 };
 
 } // namespace vesc_ackermann
--- vesc/vesc_ackermann/src/ackermann_to_vesc.cpp
+++ vesc/vesc_ackermann/src/ackermann_to_vesc.cpp
@@ -25,17 +25,24 @@ AckermannToVesc::AckermannToVesc(ros::NodeHandle nh, ros::NodeHandle private_nh)
   if (!getRequiredParam(nh, "steering_angle_to_servo_offset", steering_to_servo_offset_))
     return;
 
+  current_vel = 0.0;
+  vel_diff_thresh = 0.5;
+
   // create publishers to vesc electric-RPM (speed) and servo commands
   erpm_pub_ = nh.advertise<std_msgs::Float64>("commands/motor/speed", 10);
   servo_pub_ = nh.advertise<std_msgs::Float64>("commands/servo/position", 10);
+  brake_pub_ = nh.advertise<std_msgs::Float64>("commands/motor/brake", 10);
 
   // subscribe to ackermann topic
   ackermann_sub_ = nh.subscribe("ackermann_cmd", 10, &AckermannToVesc::ackermannCmdCallback, this);
+  odom_sub_ = nh.subscribe("odom", 10 , &AckermannToVesc::odomCallback, this);
 }
 
 typedef ackermann_msgs::AckermannDriveStamped::ConstPtr AckermannMsgPtr;
 void AckermannToVesc::ackermannCmdCallback(const AckermannMsgPtr& cmd)
 {
+  double commanded_vel = cmd->drive.speed;
+
   // calc vesc electric RPM (speed)
   std_msgs::Float64::Ptr erpm_msg(new std_msgs::Float64);
   erpm_msg->data = speed_to_erpm_gain_ * cmd->drive.speed + speed_to_erpm_offset_;
@@ -44,11 +51,28 @@ void AckermannToVesc::ackermannCmdCallback(const AckermannMsgPtr& cmd)
   std_msgs::Float64::Ptr servo_msg(new std_msgs::Float64);
   servo_msg->data = steering_to_servo_gain_ * cmd->drive.steering_angle + steering_to_servo_offset_;
 
+  // brake msg
+  std::msgs::Float64::Ptr brake_msg(new std_msgs::Float64);
+  brake_msg->data = 20000;
+
   // publish
   if (ros::ok()) {
-    erpm_pub_.publish(erpm_msg);
-    servo_pub_.publish(servo_msg);
+    if (commanded_vel > 0 && current_vel > commanded_vel + vel_diff_thresh) {
+      brake_pub_.publish(brake_msg);
+      servo_pub_.publish(servo_msg);
+    } else if (commanded_vel < 0 && current_vel < commanded_vel - vel_diff_thresh) {
+      brake_pub_.publish(brake_msg);
+      servo_pub_.publish(servo_msg);
+    } else {
+      erpm_pub_.publish(erpm_msg);
+      servo_pub_.publish(servo_msg);
+    }
   }
+}
+
+void AckermannToVesc::odomCallback(const nav_msgs::Odometry::ConstPtr& odom_msg)
+{
+  current_vel = odom_msg->twist.twist.linear.x;
 }
 
 template <typename T>
