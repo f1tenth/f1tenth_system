f18fa9ad17fef11edd50180930a0c43dc601e3d0
hikeseti101@gmail.com <hikeseti101@gmail.com>
2026-02-19 20:24:22 -0800

    Initial Video Streaming Files

---
+++ video_streaming/gstreamer_sender.cpp
@@ -0,0 +1,177 @@
+#include "gstreamer_sender.hpp"
+
+#include <iostream>
+#include <utility>
+
+static GstElement* create_video_convert(const gchar* name) {
+  GstElement* video_convert = gst_element_factory_make("nvvidconv", name);
+
+  if (!video_convert) {
+    video_convert = gst_element_factory_make("videoconvert", name);
+  }
+
+  return video_convert;
+}
+
+static GstElement* create_h264_encoder(const gchar* name) {
+  GstElement* encoder = gst_element_factory_make("nvv4l2h264enc", name);
+
+  if (!encoder) {
+    std::cout << "Using x264enc as a fallback" << std::endl;
+
+    encoder = gst_element_factory_make("x264enc", name);
+    g_object_set(encoder, "speed-preset", X264_ENC_PRESET_ULTRAFAST, NULL);
+    g_object_set(encoder, "tune", X264_ENC_TUNE_ZEROLATENCY, NULL);
+    return encoder;
+  }
+
+  std::cout << "Using nvv4l2h264enc" << std::endl;
+
+  g_object_set(encoder, "preset-level", 2, NULL);
+  g_object_set(encoder, "profile", 0, NULL);
+  g_object_set(encoder, "insert-sps-pps", true, NULL);
+  g_object_set(encoder, "poc-type", 2, NULL);
+  g_object_set(encoder, "maxperf-enable", true, NULL);
+
+  return encoder;
+}
+
+VideoOutputPipeline::VideoOutputPipeline(VideoOutputTrackInfo info)
+    : info_(info),
+      encode_caps_(gst_caps_new_empty()),
+      pipeline_(gst_pipeline_new("pipeline")),
+      bin_(GST_BIN(pipeline_)),
+      convert_(create_video_convert("convert")),
+      convert_queue_(gst_element_factory_make("queue", "convert_queue")),
+      encode_filter_(gst_element_factory_make("capsfilter", "encode_filter")),
+      encode_queue_(gst_element_factory_make("queue", "encode_queue")),
+      sink_(gst_element_factory_make("appsink", "sink")) {
+
+  // Make the queues leaky to prevent stale data from being processed when the pipeline is not consuming data fast enough.
+  g_object_set(convert_queue_, "max-size-buffers", 10, NULL);  // Keep only 3 frames
+  g_object_set(convert_queue_, "leaky", 2, NULL);  // 2 = drop old frames
+
+  g_object_set(encode_queue_, "max-size-buffers", 10, NULL);
+  g_object_set(encode_queue_, "leaky", 2, NULL);  // Drop old encoded frames
+  
+
+  switch (info_.source) {
+    case VideoSource::v4l2: {
+      filter_caps_structure_ = gst_structure_new(
+          info.media_type.c_str(), "width", G_TYPE_INT, info.caps.width,
+          "height", G_TYPE_INT, info.caps.height, "framerate",
+          GST_TYPE_FRACTION, info.caps.framerate.num, info.caps.framerate.den,
+          NULL);
+      filter_caps_ = gst_caps_new_full(filter_caps_structure_, NULL);
+      source_ = gst_element_factory_make("v4l2src", "source");
+      source_filter_ = gst_element_factory_make("capsfilter", "source_filter");
+
+      g_object_set(source_, "device", info.device.c_str(), NULL);
+
+      break;
+    }
+
+    case VideoSource::navargus: {
+      filter_caps_ = gst_caps_from_string(
+          "video/x-raw(memory:NVMM), width=240, height=120, framerate=30/1");
+      source_ = gst_element_factory_make("nvarguscamerasrc", "source");
+      source_filter_ = gst_element_factory_make("capsfilter", "source_filter");
+
+      g_object_set(source_, "sensor_id", std::stoi(info.device), NULL);
+
+      break;
+    }
+
+    case VideoSource::videotestsrc: {
+      filter_caps_structure_ = gst_structure_new(
+          info.media_type.c_str(), "width", G_TYPE_INT, info.caps.width,
+          "height", G_TYPE_INT, info.caps.height, "framerate",
+          GST_TYPE_FRACTION, info.caps.framerate.num, info.caps.framerate.den,
+          NULL);
+      filter_caps_ = gst_caps_new_full(filter_caps_structure_, NULL);
+      source_ = gst_element_factory_make("videotestsrc", "source");
+      source_filter_ = gst_element_factory_make("capsfilter", "source_filter");
+
+      break;
+    }
+  }
+
+  switch (info_.codec) {
+    case VideoCodec::H264: {
+      encode_caps_structure_ = gst_structure_from_string(
+          "video/x-h264, profile=(string)constrained-baseline", NULL);
+      gst_caps_append_structure(encode_caps_, encode_caps_structure_);
+      encode_ = create_h264_encoder("encode");
+      payload_ = gst_element_factory_make("rtph264pay", "pay");
+
+      g_object_set(encode_filter_, "caps", encode_caps_, NULL);
+      g_object_set(payload_, "pt", H264_PAYLOAD_TYPE, NULL);
+
+      break;
+    }
+
+    case VideoCodec::VP8:
+
+    default: {
+      encode_caps_structure_ = gst_structure_from_string("video/x-vp8", NULL);
+      gst_caps_append_structure(encode_caps_, encode_caps_structure_);
+      encode_ = gst_element_factory_make("vp8enc", "encode");
+      payload_ = gst_element_factory_make("rtpvp8pay", "pay");
+
+      g_object_set(encode_, "deadline", 1, NULL);
+      g_object_set(encode_filter_, "caps", encode_caps_, NULL);
+      g_object_set(payload_, "pt", VP8_PAYLOAD_TYPE, NULL);
+
+      break;
+    }
+  }
+
+  gst_bin_add_many(bin_, source_, source_filter_, NULL);
+  gst_element_link_many(source_, source_filter_, NULL);
+
+  if (!info.media_type.rfind("image/jpeg", 0)) {
+    GstElement* decode = gst_element_factory_make("jpegdec", "decode");
+    GstElement* decode_queue =
+        gst_element_factory_make("queue", "decode_queue");
+    gst_bin_add_many(bin_, decode, decode_queue, convert_, NULL);
+    gst_element_link_many(source_filter_, decode, decode_queue, convert_, NULL);
+  } else {
+    gst_bin_add_many(bin_, convert_, NULL);
+    gst_element_link(source_filter_, convert_);
+  }
+
+  g_object_set(source_filter_, "caps", filter_caps_, NULL);
+  g_object_set(payload_, "mtu", RTP_MTU, NULL);
+  g_object_set(sink_, "sync", false, NULL);
+
+  gst_bin_add_many(bin_, convert_queue_, encode_, encode_filter_, encode_queue_,
+                   payload_, sink_, NULL);
+  gst_element_link_many(convert_, convert_queue_, encode_, encode_filter_,
+                        encode_queue_, payload_, sink_, NULL);
+}
+
+VideoOutputPipeline::~VideoOutputPipeline() {
+  stop();
+  gst_object_unref(pipeline_);
+  gst_caps_unref(filter_caps_);
+  gst_caps_unref(encode_caps_);
+}
+
+void VideoOutputPipeline::start() {
+  GstStateChangeReturn ret =
+      gst_element_set_state(pipeline_, GST_STATE_PLAYING);
+  if (ret != GST_STATE_CHANGE_FAILURE) {
+    std::cout << "Successfully turned ON VIDEO pipeline" << std::endl;
+  } else {
+    std::cerr << "Failed to turn ON VIDEO pipeline" << std::endl;
+  }
+}
+
+void VideoOutputPipeline::stop() {
+  GstStateChangeReturn ret = gst_element_set_state(pipeline_, GST_STATE_NULL);
+  if (ret != GST_STATE_CHANGE_FAILURE) {
+    std::cout << "Successfully turned OFF VIDEO pipeline" << std::endl;
+  } else {
+    std::cerr << "Failed to turn OFF VIDEO pipeline" << std::endl;
+  }
+}
---
+++ video_streaming/gstreamer_sender.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#define H264_PAYLOAD_TYPE 102
+#define VP9_PAYLOAD_TYPE 100
+#define VP8_PAYLOAD_TYPE 96
+#define X264_ENC_PRESET_ULTRAFAST 1
+#define X264_ENC_TUNE_ZEROLATENCY 4
+#define RTP_MTU 1200
+
+#include <gst/gst.h>
+
+#include <string>
+
+struct AVRational {
+  int num;
+  int den;
+};
+
+enum class VideoCodec {
+  H264 = 0,
+  VP8 = 1,
+};
+
+enum class VideoSource {
+  v4l2 = 0,
+  navargus = 1,
+  videotestsrc = 2,
+};
+
+struct VideoCaps {
+  int width;
+  int height;
+  AVRational framerate;
+};
+
+struct VideoOutputTrackInfo {
+  VideoSource source;
+  std::string device;
+  std::string media_type;
+  VideoCaps caps;
+  VideoCodec codec;
+};
+
+/**
+ * A GStreamer pipeline that encodes a video stream from a media device.
+ */
+class VideoOutputPipeline {
+ public:
+  VideoOutputPipeline(VideoOutputTrackInfo info);
+
+  ~VideoOutputPipeline();
+
+  void start();
+
+  void stop();
+
+  inline GstElement* get_sink() const { return sink_; }
+
+ private:
+  VideoOutputTrackInfo info_;
+
+  GstStructure* filter_caps_structure_;
+  GstCaps* filter_caps_;
+  GstStructure* encode_caps_structure_;
+  GstCaps* encode_caps_;
+
+  GstElement* pipeline_;
+  GstBin* bin_;
+
+  GstElement* source_;
+  GstElement* source_filter_;
+  GstElement* decode_;
+  GstElement* convert_;
+  GstElement* convert_queue_;
+  GstElement* encode_;
+  GstElement* encode_filter_;
+  GstElement* encode_queue_;
+  GstElement* payload_;
+  GstElement* sink_;
+};
