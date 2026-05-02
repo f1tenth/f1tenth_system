94cb8d7fb5439315316bf80aadbc7256b80cb4e2
Ahmad Amine <ahmadamine998@gmail.com>
2026-05-02 02:23:44 -0400

    Deprecate unused tf_publisher script

--- f1tenth_stack/setup.py
+++ f1tenth_stack/setup.py
@@ -26,7 +26,6 @@ setup(
     entry_points={
         'console_scripts': [
             'throttle_interpolator = f1tenth_stack.throttle_interpolator:main',
-            'tf_publisher = f1tenth_stack.tf_publisher:main'
         ],
     },
 )
